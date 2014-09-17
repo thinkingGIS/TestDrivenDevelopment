d672ba8f194f12da30977e7b54cdf6a60313f7a5
David Martínez Morata <David@MacBook-Pro-de-David.local>
2014-09-17 19:24:06 +0200

    Foreign Key added to Items and List model added in pass mode

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+
+    """docstring for List"""
+    pass
+
+    
 class Item(models.Model):
+
     """docstring for Item"""
-    text = models.TextField(default = '')
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
+
 
--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request):
-
     items = Item.objects.all()
-
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
