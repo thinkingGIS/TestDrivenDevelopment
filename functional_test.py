db6334ab80cef2a6bd24ea0c9cc022ec5bfe4ed9
David Martínez Morata <David@MacBook-Pro-de-David.local>
2014-09-17 09:26:07 +0200

    Chapter 5 First commit before refactor the functional test

--- functional_test.py
+++ functional_test.py
@@ -39,22 +39,35 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table Madafaka!"
-            )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table Madafaka! -- its text was:\n%s" % (
+        #         table.text,
+        #         )
+        #     )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # The is still a text box inviting her to add another item. She 
         # enters "Use peacock feathers to make a fly" (Edith is very 
         # methodical)
-        self.fail('Finish the test Madafaka!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+            )
 
         # Edith wonders whether the site will remember her list. Then she see 
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-
+        self.fail('Finish the test Madafaka!')
+        
         # She visits that URL - her to-to list is still there.
 
         # Satisfied, she goes back to sleep
