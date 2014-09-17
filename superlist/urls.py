9aba73f62096c5caba5e56448bc9a7eb05a720c0
David Martínez Morata <David@MacBook-Pro-de-David.local>
2014-09-17 16:16:42 +0200

    New URL, view and template to display lists

--- superlist/urls.py
+++ superlist/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'superlist.views.home', name='home'),
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+        ),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
