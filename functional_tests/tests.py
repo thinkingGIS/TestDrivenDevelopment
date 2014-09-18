29d6c7f4441500a36cfb279a7c9425b553d13cab
David Martínez Morata <David@MacBook-Pro-de-David.local>
2014-09-18 12:02:17 +0200

    Set STATIC_ROOT in settings and disable admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,17 @@ import unittest
 class NewVisitorTest(StaticLiveServerTestCase):
     """docstring for NewVisitorTest"""
 
+    @classmethod
+    class setUpClass(cls):
+        """docstring for setUpClass"""
+        for arg in sys.arg:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+            
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -22,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -85,7 +96,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page. There is no sign of Edith's
         #list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -113,7 +124,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # She visits that URL - her to-to list is still there.
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         # She notice the input box is nicely centered
