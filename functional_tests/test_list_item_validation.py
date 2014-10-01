377519aaec29e0d1ac6032432fcfa2c880501ee3
David Martínez Morata <David@MacBook-Pro-de-David.local>
2014-10-01 09:51:28 +0200

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,19 +15,19 @@ class ItemValidationTest(FunctionalTest):
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can´t have an empty list item Madafaka!")
+        self.assertEqual(error.text, "You can't have an empty list item Madafaka!")
 
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
-        self.browser,find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # She recives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You con't have an empty list item Madafaka!")
+        self.assertEqual(error.text, "You can't have an empty list item Madafaka!")
 
         # And she can correct it by filling some text in
         self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
